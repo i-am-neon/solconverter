1a5bd21ca578ea340c8a5d666f77228fd490e4d2
0xNeon-opensource <90662438+0xNeon-opensource@users.noreply.github.com>
2022-03-24 13:00:55 -0400

    Clean up nav bar

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,18 +2,6 @@ import { Disclosure } from "@headlessui/react";
 import { SwitchHorizontalIcon } from "@heroicons/react/outline";
 import NextLink from "./NextLink";
 
-const navigation = [
-    { name: 'Dashboard', href: '#', current: true },
-    { name: 'Team', href: '#', current: false },
-    { name: 'Projects', href: '#', current: false },
-    { name: 'Calendar', href: '#', current: false },
-    { name: 'Reports', href: '#', current: false },
-]
-
-function classNames(...classes: string[]) {
-    return classes.filter(Boolean).join(' ')
-}
-
 export default function Header() {
     return (
         <div className="pb-32">
@@ -44,21 +32,14 @@ export default function Header() {
 
                                         <div className="hidden md:block">
                                             <div className="ml-10 flex items-baseline space-x-4">
-                                                {navigation.map((item) => (
-                                                    <a
-                                                        key={item.name}
-                                                        href={item.href}
-                                                        className={classNames(
-                                                            item.current
-                                                                ? 'bg-gray-900 text-white'
-                                                                : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                                                            'px-3 py-2 rounded-md text-sm font-medium'
-                                                        )}
-                                                        aria-current={item.current ? 'page' : undefined}
-                                                    >
-                                                        {item.name}
-                                                    </a>
-                                                ))}
+                                                <a
+                                                    href=""
+                                                    target='_blank'
+                                                    rel="noreferrer"
+                                                    className="px-3 py-2 rounded-md text-sm font-medium hover:bg-gray-700"
+                                                >
+                                                    Arweave Converter
+                                                </a>
                                             </div>
                                         </div>
                                     </div>
