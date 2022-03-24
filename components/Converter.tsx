332f9eda94673026f4a086215c4f8a67a894fdfc
0xNeon-opensource <90662438+0xNeon-opensource@users.noreply.github.com>
2022-03-24 11:27:49 -0400

    Add Sol to Lamport conversion

--- components/Converter.tsx
+++ components/Converter.tsx
@@ -1,13 +1,26 @@
 import { SwitchHorizontalIcon } from "@heroicons/react/outline";
 import InputKeyboardShortcut from "./InputKeyboardShortcut";
+import { LAMPORTS_PER_SOL } from '@solana/web3.js';
+import { useState } from "react";
 
 export default function Converter() {
+    const [sol, setSol] = useState(1);
+    const [lamports, setLamports] = useState(1 * LAMPORTS_PER_SOL);
+
+    const updateFromSol = (newSolValue: number): void => {
+        console.log('newSolValue :>> ', newSolValue);
+        setSol(newSolValue);
+        setLamports(newSolValue * LAMPORTS_PER_SOL);
+    }
+
     return (
         <div className="w-4/5 content-center">
             <div className="columns-3">
                 <InputKeyboardShortcut
                     label='Sol'
                     keyboardShortcut='⌘K'
+                    initialValue={sol}
+                    updateFn={updateFromSol}
                 />
                 <br />
                 <SwitchHorizontalIcon
@@ -17,6 +30,8 @@ export default function Converter() {
                 <InputKeyboardShortcut
                     label='Lamports'
                     keyboardShortcut='⌘⇧K'
+                    initialValue={lamports}
+                    updateFn={updateFromSol}
                 />
             </div>
         </div>
--- components/InputKeyboardShortcut.tsx
+++ components/InputKeyboardShortcut.tsx
@@ -1,21 +1,26 @@
+import { SyntheticEvent } from "react"
 
 type InputKeyboardShortcutProps = {
     label: string,
-    keyboardShortcut: string
+    keyboardShortcut: string,
+    initialValue: number,
+    updateFn: any
 }
 
 export default function InputKeyboardShortcut(props: InputKeyboardShortcutProps) {
     return (
       <div>
-        <label htmlFor="search" className="block text-sm font-medium text-gray-700">
+        <label htmlFor="search" className="block text-sm font-medium text-white">
           {props.label}
         </label>
         <div className="mt-1 relative flex items-center">
           <input
-            type="text"
+            type="number"
             name={props.label}
             id={props.label}
             autoComplete="off"
+            defaultValue={props.initialValue}
+            onChange={(e: any) => props.updateFn(e.target.value)}
             className="shadow-sm focus:ring-solana-green focus:border-solana-green block w-full pr-12 sm:text-sm border-gray-300 rounded-md"
           />
           <div className="absolute inset-y-0 right-0 flex py-1.5 pr-1.5">
