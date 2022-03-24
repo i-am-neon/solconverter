a1ff0a79c668cd0efb71f83128afeee82af7d83e
0xNeon-opensource <90662438+0xNeon-opensource@users.noreply.github.com>
2022-03-24 12:08:46 -0400

    Update copy notification styling

--- components/CopyNotification.tsx
+++ components/CopyNotification.tsx
@@ -24,30 +24,18 @@ export default function CopyNotification(props: CopyNotificationProps) {
             enter="transform ease-out duration-300 transition"
             enterFrom="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
             enterTo="translate-y-0 opacity-100 sm:translate-x-0"
-            leave="transition ease-in duration-100"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
+            leave="transform ease-in duration-300 transition"
+            leaveFrom="translate-y-0 opacity-100 sm:translate-x-0"
+            leaveTo="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
           >
-            <div className="max-w-sm w-full bg-white shadow-lg rounded-lg pointer-events-auto ring-1 ring-black ring-opacity-5 overflow-hidden">
+            <div className="w-32 bg-gray-700 shadow-lg rounded-lg pointer-events-auto ring-1 ring-black ring-opacity-5 overflow-hidden">
               <div className="p-4">
                 <div className="flex items-start">
                   <div className="flex-shrink-0">
-                    <CheckCircleIcon className="h-6 w-6 text-green-400" aria-hidden="true" />
+                    <CheckCircleIcon className="h-6 w-6 text-solana-green" aria-hidden="true" />
                   </div>
                   <div className="ml-3 w-0 flex-1 pt-0.5">
-                    <p className="text-sm font-medium text-gray-900">Successfully saved!</p>
-                    <p className="mt-1 text-sm text-gray-500">Anyone with a link can now view this file.</p>
-                  </div>
-                  <div className="ml-4 flex-shrink-0 flex">
-                    <button
-                      className="bg-white rounded-md inline-flex text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                      onClick={() => {
-                        props.setShow(false)
-                      }}
-                    >
-                      <span className="sr-only">Close</span>
-                      <XIcon className="h-5 w-5" aria-hidden="true" />
-                    </button>
+                    <p className="text-sm font-medium text-white">Copied!</p>
                   </div>
                 </div>
               </div>
