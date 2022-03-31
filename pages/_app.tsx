91a974e0f1aa6cc372b959c092defbbd3a37be9f
0xNeon-opensource <90662438+0xNeon-opensource@users.noreply.github.com>
2022-03-30 20:46:58 -0500

    Next pass at google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,10 +7,12 @@ import { useEffect } from 'react'
 
 const App = ({ Component, pageProps }: AppProps) => {
   const router = useRouter()
+
   useEffect(() => {
-    const handleRouteChange = (url: string) => {
+    const handleRouteChange = (url: any) => {
       gtag.pageview(url)
     }
+
     router.events.on('routeChangeComplete', handleRouteChange)
     return () => {
       router.events.off('routeChangeComplete', handleRouteChange)
@@ -19,27 +21,17 @@ const App = ({ Component, pageProps }: AppProps) => {
 
   return (
     <>
-      {/* Global Site Tag (gtag.js) - Google Analytics */}
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-      />
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${gtag.GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-        }}
-      />
+      <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`} strategy='afterInteractive' />
+      <Script id="google-analytics" strategy='afterInteractive'>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.NEXT_PUBLIC_MEASUREMENT_ID}');
+        `}
+      </Script>
       <Component {...pageProps} />
-    </>
-  )
+    </>)
 }
 export default App
